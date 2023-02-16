6b90152ea822670f336c215f5ccbae0164679b54
César Mayta <cesarmayta@gmail.com>
2023-02-16 00:07:43 -0500

    modulo 1 listo

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria,Producto
+
+admin.site.register(Categoria)
+#admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','precio','categoria','fecha_registro')
+    list_editable = ('precio',)
