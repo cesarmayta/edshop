65ae870046321321d8cf299af6353577e8e5af43
César Mayta <cesarmayta@gmail.com>
2023-02-17 00:40:43 -0500

    modulo 2

--- web/views.py
+++ web/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+
+from .models import Categoria,Producto
 
 # Create your views here.
+""" VISTAS PARA EL CATALOGO DE PRODUCTOS """
+
+def index(request):
+    listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
+    
+    #print(listaProductos)
+    context = {
+        'productos':listaProductos,
+        'categorias':listaCategorias
+    }
+    return render(request,'index.html',context)
+
+def productosPorCategoria(request,categoria_id):
+    """ vista para filtrar productos por categoria """
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+    
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+def productosPorNombre(request):
+    """ vista para filtrado de productos por nombre """
+    nombre = request.POST['nombre']
+    
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+    listaCategorias = Categoria.objects.all()
+    
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    
+    return render(request,'index.html',context)
+
+def productoDetalle(request,producto_id):
+    """ vista para el detalle de producto"""
+    
+    #objProducto = Producto.objects.get(pk=producto_id)
+    objProducto = get_object_or_404(Producto,pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    
+    return render(request,'producto.html',context)
